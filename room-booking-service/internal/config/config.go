c8c70c0fcefed8054b57d8fb1dae809d28bb7fc7
Кирилл Вешняков <kirillveshnakov@gmail.com>
2026-08-02 06:43:15 +0300

    add db params in config

--- room-booking-service/internal/config/config.go
+++ room-booking-service/internal/config/config.go
@@ -1,1 +1,25 @@
 package config
+
+import (
+	"fmt"
+	"net"
+)
+
+type Config struct {
+	PG struct {
+		Host     string `env:"POSTGRES_HOST" envDefault:"localhost"`
+		Port     string `env:"POSTGRES_PORT" envDefault:"5432"`
+		DB       string `env:"POSTGRES_DB" envDefault:"meeting_rooms"`
+		User     string `env:"POSTGRES_USER" envDefault:"meeting_rooms_user"`
+		Password string `env:"POSTGRES_PASSWORD" envDefault:"12345"`
+	}
+}
+
+func (c *Config) ConstructPostgresURL() string {
+	return fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable",
+		c.PG.User,
+		c.PG.Password,
+		net.JoinHostPort(c.PG.Host, c.PG.Port),
+		c.PG.DB,
+	)
+}
