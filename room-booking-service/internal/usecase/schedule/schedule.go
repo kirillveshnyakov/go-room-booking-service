1dcd4d799f9d21d88de4e9d04dd8606add2a7ef6
Кирилл Вешняков <kirillveshnakov@gmail.com>
2026-08-22 14:56:10 +0300

    add schedule usecase

---
+++ room-booking-service/internal/usecase/schedule/schedule.go
@@ -0,0 +1,71 @@
+package schedule
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/kirillveshnyakov/go-room-booking-service/room-booking-service/internal/entity"
+	"github.com/kirillveshnyakov/go-room-booking-service/room-booking-service/internal/errs"
+	"go.uber.org/zap"
+)
+
+type (
+	scheduleRepository interface {
+		Create(ctx context.Context, schedule entity.Schedule) (entity.Schedule, error)
+	}
+)
+
+type scheduleService struct {
+	scheduleRepository scheduleRepository
+	logger             *zap.Logger
+}
+
+func NewScheduleService(
+	scheduleRepository scheduleRepository,
+	logger *zap.Logger,
+) *scheduleService {
+	return &scheduleService{
+		scheduleRepository: scheduleRepository,
+		logger:             logger.Named("schedule_usecase"),
+	}
+}
+
+func (service *scheduleService) Create(
+	ctx context.Context,
+	roomID uuid.UUID,
+	rules []entity.ScheduleRule,
+) (entity.Schedule, error) {
+	schedule := entity.Schedule{
+		RoomID: roomID,
+		Rules:  rules,
+	}
+	if err := schedule.Validate(); err != nil {
+		return entity.Schedule{}, fmt.Errorf("schedule usecase - create: validation error: %w", err)
+	}
+
+	createdSchedule, err := service.scheduleRepository.Create(ctx, schedule)
+	if err != nil {
+		if errors.Is(err, errs.ErrScheduleAlreadyExists) ||
+			errors.Is(err, errs.ErrRoomNotFound) {
+			return entity.Schedule{}, err
+		}
+
+		service.logger.Error(
+			"schedule creation failed",
+			zap.Error(err),
+		)
+
+		return entity.Schedule{}, fmt.Errorf("schedule usecase - create: %w", err)
+	}
+
+	service.logger.Info(
+		"schedule created",
+		zap.String("schedule_id", createdSchedule.ID.String()),
+		zap.String("room_id", createdSchedule.RoomID.String()),
+		zap.Int("rules_count", len(createdSchedule.Rules)),
+	)
+
+	return createdSchedule, nil
+}
